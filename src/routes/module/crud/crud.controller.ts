713ba268e67a064d443f1fb935283cf0fe9942a2
Adrian Lopez <51672928+adrihle@users.noreply.github.com>
2022-11-12 17:26:40 +0100

    feat(providers): added base crud controller (#3)
    https://trello.com/c/Qp9dTmvK/11-create-base-endpoint-service-provider

---
+++ src/routes/module/crud/crud.controller.ts
@@ -0,0 +1,37 @@
+import { Body, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { RepositoryService } from '@repository';
+import { CrudService } from './crud.service';
+
+const BASE_CRUD = 'crud';
+const PARAM_ID_BASE_CRUD = `${BASE_CRUD}/:id`;
+
+export class CrudController<T> extends CrudService<T> {
+  constructor(private readonly repository: RepositoryService<T>) {
+    super(repository);
+  }
+
+  @Get(PARAM_ID_BASE_CRUD)
+  crudGet(@Param('id') id: string) {
+    return this._get(id);
+  }
+
+  @Get(BASE_CRUD)
+  crudPagination(@Query() query: any = {}) {
+    return this._pagination(query);
+  }
+
+  @Post(BASE_CRUD)
+  crudCreate(@Body() body: T) {
+    return this._create(body);
+  }
+
+  @Put(PARAM_ID_BASE_CRUD)
+  crudUpdate(@Param('id') id: string, @Body() body: Partial<T>) {
+    return this._update(id, body);
+  }
+
+  @Delete(PARAM_ID_BASE_CRUD)
+  crudRemove(@Param('id') id: string) {
+    return this._remove(id);
+  }
+}
--- src/routes/user/user.service.ts
+++ src/routes/user/user.service.ts
@@ -1,26 +1,4 @@
-import { Inject, Injectable } from '@nestjs/common';
-import { User, UserRepository } from '@repository';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UserService {
-  @Inject()
-  private readonly repository: UserRepository;
-
-  async list(query: any = {}) {
-    const { page, offset } = query;
-    return this.repository.pagination({
-      pagination: {
-        page,
-        offset,
-      },
-    });
-  }
-
-  async get(id: string) {
-    return this.repository.lookById(id);
-  }
-
-  async create(user: User) {
-    return this.repository.create(user);
-  }
-}
+export class UserService {}
